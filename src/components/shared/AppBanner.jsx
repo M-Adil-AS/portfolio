8d9810963db1653760fd99f23e126bd3d0796a74
unknown <muhammadadilahmadsiddiqui90@gmail.com>
2023-07-05 01:07:20 +0500

    Download CV dark mode fixed

--- src/components/shared/AppBanner.jsx
+++ src/components/shared/AppBanner.jsx
@@ -66,17 +66,29 @@ const AppBanner = ({activeTheme, setTheme}) => {
 					}}
 					className="flex justify-center sm:block"
 				>
+					{activeTheme==='light' ? 
 					<a
 						download="adilCV.pdf"
 						href="/files/adilCV.pdf"
-						className="font-general-medium flex justify-center items-center w-36 sm:w-48 mt-12 mb-6 sm:mb-0 text-lg border border-indigo-200 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500 controlPadding"
+						className="font-general-medium flex justify-center items-center w-36 sm:w-48 mt-12 mb-6 sm:mb-0 text-lg border dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg customBg focus:ring-1 focus:ring-indigo-900 duration-500 controlPadding"
 						aria-label="Download Resume"
 					>
 						<FiArrowDownCircle className="mr-2 sm:mr-3 h-5 w-5 sn:w-6 sm:h-6 duration-100"></FiArrowDownCircle>
 						<span className="text-sm sm:text-lg font-general-medium duration-100">
 							Download CV
 						</span>
-					</a>
+					</a>:
+					<a
+						download="adilCV.pdf"
+						href="/files/adilCV.pdf"
+						className="font-general-medium flex justify-center items-center w-36 sm:w-48 mt-12 mb-6 sm:mb-0 text-lg border border-grey-500 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 hover:bg-indigo-500 focus:ring-1 focus:ring-indigo-900 text-gray-500 hover:text-white duration-500 controlPadding"
+						aria-label="Download Resume"
+					>
+						<FiArrowDownCircle className="mr-2 sm:mr-3 h-5 w-5 sn:w-6 sm:h-6 duration-100"></FiArrowDownCircle>
+						<span className="text-sm sm:text-lg font-general-medium duration-100">
+							Download CV
+						</span>
+					</a>}
 				</motion.div>
 			</div>
 			<motion.div
